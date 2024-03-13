e502d5dd6d5b028de2c30e85d8cbea74baa68361
RicoBasyar <RicoBasyar@users.noreply.github.com>
2024-03-14 03:20:27 +0700

    Add Student Data Service

--- src/main/java/com/javaspringtest/testcrud/repository/StudentRepository.java
+++ src/main/java/com/javaspringtest/testcrud/repository/StudentRepository.java
@@ -2,7 +2,10 @@ package com.javaspringtest.testcrud.repository;
 
 import com.javaspringtest.testcrud.entity.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 public interface StudentRepository extends JpaRepository<StudentEntity, Integer> {
+    @Query("SELECT e FROM StudentEntity e where e.StudentName = :StudentName")
+    StudentEntity findByName(String name);
 }
---
+++ src/main/java/com/javaspringtest/testcrud/service/StudentService.java
@@ -0,0 +1,33 @@
+package com.javaspringtest.testcrud.service;
+
+import com.javaspringtest.testcrud.entity.StudentEntity;
+import com.javaspringtest.testcrud.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository repository;
+
+    public StudentEntity addStudent(StudentEntity student){
+        return repository.save(student);
+    }
+
+    public StudentEntity getStudentById(int id){
+        return repository.findById(id).orElse(null);
+    }
+
+    public StudentEntity getStudentByName(String name){
+        return repository.findByName(name);
+    }
+
+    public StudentEntity updateStudent(StudentEntity student){
+        StudentEntity ExistingStudentEntity = repository.findById(student.getStudentID()).orElse(null);
+        ExistingStudentEntity.setStudentName(student.getStudentName());
+        ExistingStudentEntity.setStudentAge(student.getStudentAge());
+        ExistingStudentEntity.setStudentYear(student.getStudentYear());
+        return repository.save(ExistingStudentEntity);
+    }
+}
