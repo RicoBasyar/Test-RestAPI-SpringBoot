0404117fb0c74fb5b2ee91801ab305a09923588d
RicoBasyar <RicoBasyar@users.noreply.github.com>
2024-03-15 21:32:54 +0700

    Add Student Controller and Connect it to Service

--- src/main/java/com/javaspringtest/testcrud/repository/StudentRepository.java
+++ src/main/java/com/javaspringtest/testcrud/repository/StudentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+// Nanti Coba pake Folder DTO
 public interface StudentRepository extends JpaRepository<StudentEntity, Integer> {
     @Query("SELECT e FROM StudentEntity e where e.StudentName = :name")
     StudentEntity findByName(String name);
